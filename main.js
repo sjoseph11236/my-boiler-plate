2357afe4da2883a10748fce61f98c6af10325890
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-02-09 15:27:29 -0500

    chore: server working

---
+++ main.js
@@ -0,0 +1,4 @@
+const app = require('./server/server');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`App is listening on PORT ${PORT}`));
---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+// Create an express instance
+const app = express();
+
+// Logging middleware
+app.use(morgan('dev'));
+
+// Parsing middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Static middleware
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => { 
+  res.send('hello World');
+});
+
+// 404 
+app.use(function(req, res, next) {
+  const err = new Error('not found.');
+  err.status = 404; 
+  next(err);
+});
+
+// 500
+app.use((err, req, res) => {
+  console.error('This is the error from server.js ', err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal servers error ');
+});
+
+module.exports = app;

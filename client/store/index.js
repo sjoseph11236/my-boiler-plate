390238e31df756b6d7497fcfed86dfd933905719
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-02-09 16:44:51 -0500

    chore: fix combined reducer

--- client/store/index.js
+++ client/store/index.js
@@ -2,9 +2,10 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension'
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
+import  reduce  from './reducers/reduce'
 
 
-const reducer = combineReducers({})
+const reducer = combineReducers({reduce})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
---
+++ client/store/reducers/reduce.js
@@ -0,0 +1,7 @@
+const initialState = {};
+
+const reduce = (state = initialState, action) => { 
+  return state;
+}
+
+export default reduce;

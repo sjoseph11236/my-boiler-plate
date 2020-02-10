0d3d242f6e1a2bd5712458ebb85abff428191984
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-02-09 22:50:42 -0500

    feat: add jest and enzyme

---
+++ client/components/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import App from './App';
+
+describe('test', () => {
+  it('It should be 1 ', ()=> {
+    expect(1).toBe(1);
+  });
+});

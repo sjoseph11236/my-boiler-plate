8422afff5253da736d1c69c171e656636e64a7f2
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-02-21 12:27:44 -0500

    add seed file set up

---
+++ bin/seed.js
@@ -0,0 +1,21 @@
+const { db } = require('../server/db');
+const {green, red} = require('chalk');
+
+const seed = async() => {
+  await db.sync({force: true});
+
+
+
+
+
+
+  console.log(green('Seeding success!'));
+  db.close();
+}
+
+seed()
+  .catch(err => {
+    console.error(red('Oh noes! Something went wrong!'));
+    console.error(err);
+    db.close();
+  });

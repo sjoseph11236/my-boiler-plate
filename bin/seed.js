9de73a106e01c4f2de654f13253e77c8a51b59d4
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-03-01 22:19:46 -0500

    chore: add user.json to bin

--- bin/seed.js
+++ bin/seed.js
@@ -1,14 +1,12 @@
-const { db } = require('../server/db');
+const { db, User } = require('../server/db');
+const users = require('./data/user.json');
 const {green, red} = require('chalk');
 
 const seed = async() => {
   await db.sync({force: true});
-
-
-
-
-
-
+  
+  await User.bulkCreate(users);
+  
   console.log(green('Seeding success!'));
   db.close();
 }
